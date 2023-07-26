a88edad528e6416ee06a689f0724dba2a33d5756
mileydyforero <mileydyforeronendoza@gmail.com.com>
2023-07-25 20:41:17 -0500

    clase1

---
+++ js/tipoDatos.js
@@ -0,0 +1,40 @@
+// string
+
+var letra = "letra";
+console.log(typeof letra);
+
+//number
+
+var numero = 12;
+console.log(typeof numero);
+
+//boolean
+var boolean = false;
+console.log(typeof boolean);
+
+//undefined
+var undefined;
+console.log(typeof undefined);
+
+//null
+// var null= null;
+// console.log(typeof.null);
+
+//object
+var object ={
+    nombre:'mimi',
+    apellido:"forero"
+}
+
+console.log(typeof object);
+
+//arreglos
+var arreglos = ['html','css','avascript'];
+console.log(typeof arreglos);
+
+//funcion
+function mifuncion (){
+    
+}
+
+
